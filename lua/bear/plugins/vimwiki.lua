15d5c7b21c15c77faca17f23ef12d4a33bc67b14
Bear-chan <sentient.bear.chan@gmail.com>
2023-02-08 19:20:39 +0100

    Changed vimwiki path to be on windows

--- lua/bear/plugins/vimwiki.lua
+++ lua/bear/plugins/vimwiki.lua
@@ -1,1 +1,1 @@
-vim.g.vimwiki_list = { { path = "/mnt/c/Users/bear/Documents", syntax = "markdown", ext = ".md" } }
+vim.g.vimwiki_list = { { path = "/mnt/c/Users/bear/Documents/Notes", syntax = "markdown", ext = ".md" } }
